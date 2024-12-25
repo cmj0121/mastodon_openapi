9945016932bcc31e92dd8c0eb2725f1e6753d9d3
cmj <cmj@cmj.tw>
2024-12-25 21:34:41 +0800

    feat(tool): fetch the nested component if possible

--- src/handler/components.py
+++ src/handler/components.py
@@ -64,32 +64,56 @@ def handle_component(link: str) -> dict[str, ResponseObject | ReferenceObject]:
     spec = {}
     soup = BeautifulSoup(response.text, "html.parser")
 
-    schema_object = SchemaObject(type="object", properties={})
-    main_attr_dom = soup.find("a", href="#attributes").find_next("ul").findChildren("li", recursive=False)
+    toc = soup.find("nav", {"id": "TableOfContents"})
+    attrs = toc.find_next("ul").find_next("li").find_next("ul").findChildren("li", recursive=False)
 
-    for attr_dom in main_attr_dom:
-        logger.debug(f"processing component {attr_dom.text=}")
+    for attr in reversed(attrs):
+        entity_dom = attr.find("a")
+        if not (entity_dom.text == "Attributes" or entity_dom.text.endswith("attributes")):
+            logger.debug(f"skip the handle component {entity_dom.text=}")
+            continue
 
-        attr_id = attr_dom.find("a").get("href")[1:]
-        attr_name = attr_dom.find("code").text
+        schema_object = SchemaObject(type="object", properties={})
+        attrs_dom = entity_dom.parent.find_all("li")
 
-        text = soup.find("h3", {"id": attr_id}).find_next("p").text
-        matched = re.search(r"Description:([\s\S]*?)Type: (nullable )?(\S+)(?: .*?)?(?:Version history:)?(.+?)", text)
-        if not matched:
-            raise ValueError(f"failed to find the attribute {text=}")
+        for attr_dom in attrs_dom:
+            logger.debug(f"processing component {attr_dom.text=}")
 
-        desc, nullable, typ, version = matched.groups()
-        schema_object.properties[attr_name] = SchemaObject(
-            type=typ,
-            nullable=True if nullable else None,
-            description=desc.strip(),
+            attr_id = attr_dom.find("a").get("href")[1:]
+            attr_name = attr_dom.find("code").text
+
+            text = soup.find("h3", {"id": attr_id}).find_next("p").text
+            matched = re.search(
+                r"Description:([\s\S]*?)Type: (nullable )?(\S+)(?: .*?)?(?:Version history:)?(.+?)", text
+            )
+            if not matched:
+                raise ValueError(f"failed to find the attribute {text=}")
+
+            desc, nullable, typ, version = matched.groups()
+            schema_object.properties[attr_name] = SchemaObject(
+                type=typ,
+                nullable=True if nullable else None,
+                description=desc.strip(),
+            )
+
+        match entity_dom.text:
+            case "Attributes":
+                main_resp_dom = soup.find("h1")
+                main_desc_dom = main_resp_dom.find_next("p")
+
+                name = main_resp_dom.text
+                desc = main_desc_dom.text
+            case _:
+                matched = re.search(r"^([\w:]+?) (?:entity )?attributes", entity_dom.text)
+                if not matched:
+                    raise ValueError(f"cannot find the entity name {entity_dom.text=}")
+
+                (name,) = matched.groups()
+                desc = ""
+
+        spec[canonicalize(name)] = ResponseObject(
+            description=desc,
+            content={"application/json": MediaTypeObject.model_validate({"schema": schema_object})},
         )
 
-    main_resp_dom = soup.find("h1")
-    main_desc_dom = main_resp_dom.find_next("p")
-
-    spec[canonicalize(main_resp_dom.text)] = ResponseObject(
-        description=main_desc_dom.text,
-        content={"application/json": MediaTypeObject.model_validate({"schema": schema_object})},
-    )
     return spec
--- src/handler/test_components.py
+++ src/handler/test_components.py
@@ -24,3 +24,35 @@ class TestHandleComponent:
             assert isinstance(schema_object, SchemaObject)
             assert schema_object.type == "object"
             assert schema_object.items is None
+
+    @responses.activate
+    def test_handle_nested_component(self, load_component_html_fn):
+        component = "account"
+        link = f"https://docs.joinmastodon.org/entities/{component}/"
+        load_component_html_fn(component)
+
+        resp = handle_component(link)
+
+        assert "Account" in resp
+        for attr in {"id", "username", "acct", "url"}:
+            schema_object = resp["Account"].content["application/json"].schema_object
+            assert schema_object.type == "object"
+            assert attr in schema_object.properties
+
+        assert "CredentialAccount" in resp
+        for attr in {"source", "role"}:
+            schema_object = resp["CredentialAccount"].content["application/json"].schema_object
+            assert schema_object.type == "object"
+            assert attr in schema_object.properties
+
+        assert "MutedAccount" in resp
+        for attr in {"mute_expires_at"}:
+            schema_object = resp["MutedAccount"].content["application/json"].schema_object
+            assert schema_object.type == "object"
+            assert attr in schema_object.properties
+
+        assert "Field" in resp
+        for attr in {"name", "value", "verified_at"}:
+            schema_object = resp["Field"].content["application/json"].schema_object
+            assert schema_object.type == "object"
+            assert attr in schema_object.properties
